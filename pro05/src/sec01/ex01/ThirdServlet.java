3893fbe6f10c065ecb056dbb106eb5d036c94e3f
ashram7 <ashram7@hanmail.net>
2022-09-07 09:49:14 +0900

    pro06

--- pro05/src/sec01/ex01/ThirdServlet.java
+++ pro05/src/sec01/ex01/ThirdServlet.java
@@ -8,9 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class ThirdServlet
- */
+/* 프로그램의 소스코드 안에 다른 프로그램을 위한 정보를 미리 약속된 형식으로 포함시킨 것이 바로 애너테이션 */
 @WebServlet("/third")
 public class ThirdServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
